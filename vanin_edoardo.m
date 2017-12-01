333749eebc64e2926770d32119e86d9e27cf5a0d
Edoardo Vanin <edoardo.vanin@gmail.com>
2017-12-01 18:17:14 +0100

    Added the second filter for estract the second carrier

--- vanin_edoardo.m
+++ vanin_edoardo.m
@@ -25,6 +25,41 @@ disp(freq);
 disp(A1);
 disp(A2);
 
+%%%%%%% Calcolo parametri filtro passa banda per estrarre la portante
+
+f_3_dB = 1;                 %Frequenza di taglio in Hz
+theta_3_dB = 2*pi*f_3_dB/F; %Pulsazione di taglio
+
+delta = theta_3_dB / 2;
+r = 1 - delta;
+b0 = delta;
+
+a1 = zeros(2);
+a2 = zeros(2);
+
+for i = 1:2 
+    a1(i) = 2*r*cos(2*pi*freq(i)/F);
+    a2(i) = -r*r;
+end
+[H_1, w] = freqz(b0*[1 -2 1], [1 -a1(1) -a2(1)], 'whole', 2048, F);
+[H_2, w] = freqz(b0*[1 -2 1], [1 -a1(2) -a2(2)], 'whole', 2048, F);
+
+carry1 = filter(b0*[1 -2 1], [1 -a1(1) -a2(1)], signal_109);
+carry2 = filter(b0*[1 -2 1], [1 -a1(2) -a2(2)], signal_109);
+
+CARRY1 = fft(carry1(1:N));
+CARRY1_NORM = CARRY1 / N;
+CARRY2 = fft(carry2(1:N));
+CARRY2_NORM = CARRY2 / N;
+
+
+sound(carry1, F);
+sound(carry2, F);
+
+
+
+
+
 % Plot the magnitude
 figure(1)                       % Magnitude in dB (it is more meaningful)
 f=linspace(0,F,N);              % frequency axis: 0---F Hz
@@ -34,34 +69,19 @@ xlabel(' f (Hz)'); ylabel('|X_norm(f)|  (dB)');
 axis([0 F -220 -20]);
 
 
-%%%%%%% Calcolo parametri filtro passa banda per estrarre la portante
-
-f_3_dB = 1;                 %Frequenza di taglio in Hz
-theta_3_dB = 2*pi*f_3_dB/F; %Pulsazione di taglio
-
-delta = theta_3_dB / 2;
-r = 1 - delta;
-b0 = delta;
-a1 = 2*r*cos(2*pi*freq(1)/F);
-a2 = -r*r;
-
-[H_1, w] = freqz(b0*[1 -2 1], [1 -a1 -a2], 'whole', 2048, F);
-
-carry1 = filter(b0*[1 -2 1], [1 -a1 -a2], signal_109);
-
-CARRY1 = fft(carry1(1:N));
-
-%sound(carry1, F);
-
-
 figure(2)                       % Magnitude in dB (it is more meaningful)
 f=linspace(0,F,N);              % frequency axis: 0---F Hz
-plot(f,20*log10(abs(CARRY1)));
+plot(f,20*log10(abs(CARRY1_NORM)));
 title('Magnitude (in dB) of the spectrum of the signal');
 xlabel(' f (Hz)'); ylabel('|X_norm(f)|  (dB)');
 axis([0 F -220 -20]);
 
-
+figure(3)                       % Magnitude in dB (it is more meaningful)
+f=linspace(0,F,N);              % frequency axis: 0---F Hz
+plot(f,20*log10(abs(CARRY2_NORM)));
+title('Magnitude (in dB) of the spectrum of the signal');
+xlabel(' f (Hz)'); ylabel('|X_norm(f)|  (dB)');
+axis([0 F -220 -20]);
 
 
 
